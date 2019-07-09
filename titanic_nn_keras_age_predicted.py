6017c61c3df06b28b57aa42f2d352e3e373dea7d
GlaIZier <kh.mv@bk.ru>
2019-07-09 10:28:09 +0300

    titanic_svm has been refactored

--- titanic_nn_keras_age_predicted.py
+++ titanic_nn_keras_age_predicted.py
@@ -363,10 +363,10 @@ x_test = remove_is_test(x_test)
 # show_data(y_val, 'y_val')
 # show_data(x_test, 'x_test')
 
-# Todo try different models (neural_network.MLPClassifier.html, ExtraTreesClassifier)
+# Todo try different models (neural_network.MLPClassifier.html, ExtraTreesClassifier, SGDClassifier)
 # Todo try bagging and boosting
-# Todo analyse data with plots and pandas again
-# Todo implement saving the best model here
+# Todo analyse data with plots and pandas again (SequentialFeatureSelector)
+# Todo implement saving the best model here (SelectFromModel, make_pipeline)
 # Todo implement NN and other models with less features
 # Todo implement NN and other models without feature engineering
 # Todo reach 90%
--- titanic_svm.py
+++ titanic_svm.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 
 import feature_engineering as fe
 
@@ -12,20 +12,20 @@ train_border_index = 891
 validation_border_index = 265
 
 
-# accuracy ~74 (LinearSVC), ~69 (Multinomial), ~79 (Bernoulli)
-def svm_data(data, classifier=LinearSVC()):
-    accuracy = svm(data.x_train, data.y_train, data.x_val, data.y_val, classifier)
+# accuracy ~82 (linear),
+def svm_data(data, kernel='linear'):
+    accuracy = svm(data.x_train, data.y_train, data.x_val, data.y_val, kernel)
     return accuracy
 
 
-def svm(x_train, y_train, x_val, y_val, classifier=LinearSVC()):
-    gaussian_naive_bayes = classifier
+def svm(x_train, y_train, x_val, y_val, kernel='linear'):
+    gaussian_naive_bayes = SVC(kernel=kernel)
     gaussian_naive_bayes.fit(x_train, y_train)
     return gaussian_naive_bayes.score(x_val, y_val)
 
 
-# accuracy ~80 (LinearSVC), ~73 (Multinomial), ~80 (Bernoulli)
-def svm_cross_validation_manual(data, splits=5, classifier=LinearSVC()):
+# accuracy ~83 (linear)
+def svm_cross_validation_manual(data, splits=5, kernel='linear'):
     kf = KFold(n_splits=splits)
     accuracy = 0
     for train_indexes, val_indexes in kf.split(data.x_train_full):
@@ -33,15 +33,15 @@ def svm_cross_validation_manual(data, splits=5, classifier=LinearSVC()):
         y_train = data.y_train_full.iloc[train_indexes]
         x_val = data.x_train_full.iloc[val_indexes]
         y_val = data.y_train_full.iloc[val_indexes]
-        accuracy += svm(x_train, y_train, x_val, y_val, classifier)
+        accuracy += svm(x_train, y_train, x_val, y_val, kernel)
 
     return accuracy / splits
 
 
-# accuracy ~79 (LinearSVC)
-def svm_cross_validation(data, splits=5, classifier=LinearSVC()):
+# accuracy ~83 (linear)
+def svm_cross_validation(data, splits=5, kernel='linear'):
     skf = StratifiedKFold(n_splits=splits, shuffle=True, random_state=17)
-    results = cross_val_score(classifier, data.x_train_full, data.y_train_full, cv=skf)
+    results = cross_val_score(SVC(kernel=kernel), data.x_train_full, data.y_train_full, cv=skf)
     return results.mean()
 
 
