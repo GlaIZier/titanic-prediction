034ed9b5e246f177ee4678581bee93b9ab44c47d
GlaIZier <kh.mv@bk.ru>
2019-08-10 10:04:10 +0300

    Todo has been removed

--- titanic_nn_keras_age_predicted.py
+++ titanic_nn_keras_age_predicted.py
@@ -363,7 +363,6 @@ x_test = remove_is_test(x_test)
 # show_data(y_val, 'y_val')
 # show_data(x_test, 'x_test')
 
-# Todo try bagging and boosting (xgboost)
 # Todo analyse data with plots and pandas again (SequentialFeatureSelector)
 # Todo implement saving the best model here (SelectFromModel, make_pipeline)
 # Todo implement NN and other models with less features
